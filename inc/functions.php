60854045a3b7c5995019ec519ed36998fa9474f0
zamight <cody.e.wofford@gmail.com>
2014-09-14 19:40:08 -0400

    Added generate_salt

--- inc/functions.php
+++ inc/functions.php
@@ -23,3 +23,25 @@ function validate_login($username, $password)
 {
     return false;
 }
+
+function rand_str($length)
+{
+    $list = array('A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S',
+                    'T', 'U', 'V', 'W', 'X', 'Y', 'Z', '1', '2', '3', '4', '5', '6', '7', '8', '9',
+                    'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's',
+                    't', 'u', 'v', 'w', 'x', 'y', 'z');
+    $str = '';
+
+    for($i = 0; $i <= $length; ++$i)
+    {
+        $char = mt_rand(0, count($list)-1);
+        $str .= $list[$char];
+    }
+
+    return $str;
+}
+
+function generate_salt()
+{
+    return rand_str(15);
+}
--- plugintest.php
+++ plugintest.php
@@ -4,5 +4,5 @@ $plugin->add_plugin('index_start', 'plugintest');
 
 function plugintest()
 {
-    print "index_start";
+    print generate_salt();
 }
